fe5c2bd77ffd39c05762c4c69fa02b0727350bfb
solankipratik8008 <solankipratik4094@gmail.com>
2025-01-06 19:16:40 -0500

    home page adadd

---
+++ lib/home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class myhomepage extends StatelessWidget {
+  const myhomepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: SafeArea(
+        
+        child: Scaffold(
+
+
+
+
+
+
+        ),
+      
+        
+
+
+
+
+
+        ),
+      
+      );
+
+
+
+
+
+
+    
+
+
+    
+    
+    
+    
+  
+  }
+}
